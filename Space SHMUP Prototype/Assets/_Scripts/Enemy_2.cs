b7ba1696010364138e5359393ec36ce6c8e9b12c
Tucker Amend <40192294+ThornXTormentor@users.noreply.github.com>
2019-09-15 20:18:49 -0600

    Added SHMUP
    Attempt at adding SHMUP

---
+++ Space SHMUP Prototype/Assets/_Scripts/Enemy_2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_2 : Enemy
+{
+    [Header("Set in Inspector: Enemy_2")]
+    //determines how much the sine wave will affect movement
+    public float sinEccentricity = 0.6f;
+    public float lifeTime = 10;
+
+    [Header("Set Dynamically: Enemy_2")]
+    //enemy 2 uses a sin wave to modify a 2 point linear interpolation
+    public Vector3 p0;
+    public Vector3 p1;
+    public float birthTime;
+
+    void Start()
+    {
+        p0 = Vector3.zero;
+        p0.x = -bndCheck.camWidth - bndCheck.radius;
+        p0.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        p1 = Vector3.zero;
+        p1.x = bndCheck.camWidth + bndCheck.radius;
+        p1.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        if(Random.value > 0.5f)
+        {
+            p0.x *= -1;
+            p1.x *= -1;
+        }
+
+        birthTime = Time.time;
+        
+    }
+
+    public override void Move()
+    {
+        float u = (Time.time - birthTime) / lifeTime;
+
+        if (u > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        u = u + sinEccentricity * (Mathf.Sin(u * Mathf.PI * 2));
+
+        pos = (1 - u) * p0 + u * p1;
+    }
+}
